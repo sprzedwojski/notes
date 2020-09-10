4ba09ade8adbecd92b4e8e79f5756a581f9bd252
Szymon Przedwojski <szymon.przedwojski@gmail.com>
2020-09-10 09:34:07 +0200

    Add title

--- notes-frontend/src/App.js
+++ notes-frontend/src/App.js
@@ -1,28 +1,14 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Notes from './Notes'
 
 function App() {
     return (
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Hello World
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
-        // </div>
         <div>
-            <Notes/>
+            <h1 className="title">Welcome to SzymNotes!</h1>
+            <div>
+                <Notes/>
+            </div>
         </div>
     )
 }
